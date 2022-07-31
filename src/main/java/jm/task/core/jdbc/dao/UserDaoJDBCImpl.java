4635164e5126f94433ad454da7a40bae4446040b
Nikita <nkt@rambler.ru>
2022-07-31 10:48:46 +0300

    test

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -18,40 +18,41 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() throws SQLException {
-        Connection connection = getConnect();
-        connection.setAutoCommit(false);
-        String sqlCommand = "CREATE TABLE IF NOT EXISTS Users (Id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age INT )";
-        try {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sqlCommand);
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Create UsersTable error");
-            if (connection != null) {
-                try {
-                    System.err.print("Transaction is being rolled back");
-                    connection.rollback();
-                } catch (SQLException ignored) {
+        try (Connection connection = getConnect()) {
+            connection.setAutoCommit(false);
+            String sqlCommand = "CREATE TABLE IF NOT EXISTS Users (Id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age INT )";
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(sqlCommand);
+                connection.commit();
+            } catch (SQLException e) {
+                System.out.println("Create UsersTable error");
+                if (connection != null) {
+                    try {
+                        System.err.print("Transaction is being rolled back");
+                        connection.rollback();
+                    } catch (SQLException ignored) {
+                    }
                 }
             }
         }
     }
 
     public void dropUsersTable() throws SQLException {
-        Connection connection = getConnect();
-        connection.setAutoCommit(false);
-        String sqlCommand = "DROP TABLE IF EXISTS Users";
-        try {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sqlCommand);
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Drop UsersTable error");
-            if (connection != null) {
-                try {
-                    System.err.print("Transaction is being rolled back");
-                    connection.rollback();
-                } catch (SQLException ignored) {
+        try (Connection connection = getConnect()) {
+
+            connection.setAutoCommit(false);
+            String sqlCommand = "DROP TABLE IF EXISTS Users";
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(sqlCommand);
+                connection.commit();
+            } catch (SQLException e) {
+                System.out.println("Drop UsersTable error");
+                if (connection != null) {
+                    try {
+                        System.err.print("Transaction is being rolled back");
+                        connection.rollback();
+                    } catch (SQLException ignored) {
+                    }
                 }
             }
         }
@@ -59,29 +60,29 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
         String sqlCommand = "CREATE TABLE IF NOT EXISTS Users (Id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age INT )";
-           String sqlCommand1 = "INSERT Users (id, name, lastName, age) VALUES (?, ?, ?, ?)";
-        Connection connection = getConnect();
-        connection.setAutoCommit(false);
-        try {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sqlCommand);
+        String sqlCommand1 = "INSERT Users (id, name, lastName, age) VALUES (?, ?, ?, ?)";
+        try (Connection connection = getConnect()) {
+            connection.setAutoCommit(false);
 
-            PreparedStatement ps = connection.prepareStatement(sqlCommand1);
-            ps.setString(1, null);
-            ps.setString(2, name);
-            ps.setString(3, lastName);
-            ps.setString(4, String.valueOf(age));
-            ps.executeUpdate();
-            System.out.printf("User with name %s added to database." + "\n", name);
-            connection.commit();
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(sqlCommand);
+                PreparedStatement ps = connection.prepareStatement(sqlCommand1);
+                ps.setString(1, null);
+                ps.setString(2, name);
+                ps.setString(3, lastName);
+                ps.setString(4, String.valueOf(age));
+                ps.executeUpdate();
+                System.out.printf("User with name %s added to database." + "\n", name);
+                connection.commit();
 
-        } catch (SQLException e) {
-            System.out.println("SaveUser error");
-            if (connection != null) {
-                try {
-                    System.err.print("Transaction is being rolled back");
-                    connection.rollback();
-                } catch (SQLException ignored) {
+            } catch (SQLException e) {
+                System.out.println("SaveUser error");
+                if (connection != null) {
+                    try {
+                        System.err.print("Transaction is being rolled back");
+                        connection.rollback();
+                    } catch (SQLException ignored) {
+                    }
                 }
             }
         }
@@ -89,21 +90,22 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void removeUserById(long id) throws SQLException {
         String sqlCommand = "DELETE FROM Users WHERE Id = ?";
-        Connection connection = getConnect();
-        connection.setAutoCommit(false);
-        try {
-            PreparedStatement ps = connection.prepareStatement(sqlCommand);
-            ps.setLong(1, id);
-            ps.executeUpdate();
-            connection.commit();
+        try (Connection connection = getConnect()) {
+            connection.setAutoCommit(false);
 
-        } catch (SQLException e) {
-            System.out.println("DelateUser error");
-            if (connection != null) {
-                try {
-                    System.err.print("Transaction is being rolled back");
-                    connection.rollback();
-                } catch (SQLException ignored) {
+            try (PreparedStatement ps = connection.prepareStatement(sqlCommand)) {
+                ps.setLong(1, id);
+                ps.executeUpdate();
+                connection.commit();
+
+            } catch (SQLException e) {
+                System.out.println("DelateUser error");
+                if (connection != null) {
+                    try {
+                        System.err.print("Transaction is being rolled back");
+                        connection.rollback();
+                    } catch (SQLException ignored) {
+                    }
                 }
             }
         }
@@ -128,25 +130,25 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() throws SQLException {
 
         String sqlCommand = "TRUNCATE Users";
-        Connection connection = getConnect();
-        connection.setAutoCommit(false);
-        try {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sqlCommand);
-            connection.commit();
-
-        } catch (SQLException e) {
-            System.out.println("Clean UsersTable error");
-            if (connection != null) {
-                try {
-                    System.err.print("Transaction is being rolled back");
-                    connection.rollback();
-                } catch (SQLException ignored) {
+        try (Connection connection = getConnect()) {
+            connection.setAutoCommit(false);
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(sqlCommand);
+                connection.commit();
+            } catch (SQLException e) {
+                System.out.println("Clean UsersTable error");
+                if (connection != null) {
+                    try {
+                        System.err.print("Transaction is being rolled back");
+                        connection.rollback();
+                    } catch (SQLException ignored) {
+                    }
                 }
             }
         }
     }
 }
+
 
 
 
